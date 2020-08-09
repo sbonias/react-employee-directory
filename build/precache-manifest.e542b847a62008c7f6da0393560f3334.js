52e1925e482b88d448dd31ac661bfd3bef6c3547
Sam Bonias <sbonias84@gmail.com>
2020-08-09 14:11:08 -0400

    added build pack

---
+++ build/precache-manifest.e542b847a62008c7f6da0393560f3334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed1ebd2525731ad24a3bc2611bacd34",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "267c561b978f2103305a",
+    "url": "/react-employee-directory/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "3deaf95aff5e5edf4172",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "267c561b978f2103305a",
+    "url": "/react-employee-directory/static/js/2.fb91e3ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.fb91e3ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3deaf95aff5e5edf4172",
+    "url": "/react-employee-directory/static/js/main.eabd933e.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  }
+]);
