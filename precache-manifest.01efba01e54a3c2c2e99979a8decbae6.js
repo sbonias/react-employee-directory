a534418949e1373285f7d24f3e6f73c110a213f2
Sam Bonias <sbonias84@gmail.com>
2020-08-11 10:16:23 -0400

    Updates

---
+++ precache-manifest.01efba01e54a3c2c2e99979a8decbae6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f688c750027c115565d5668ae98459",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "5809c58970ae1ae97917",
+    "url": "/react-employee-directory/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "37f78431dca34648b028",
+    "url": "/react-employee-directory/static/css/main.6cda8030.chunk.css"
+  },
+  {
+    "revision": "5809c58970ae1ae97917",
+    "url": "/react-employee-directory/static/js/2.7b0a8001.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.7b0a8001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f78431dca34648b028",
+    "url": "/react-employee-directory/static/js/main.edd1b39c.chunk.js"
+  },
+  {
+    "revision": "e82547d4095795d10a4a",
+    "url": "/react-employee-directory/static/js/runtime-main.93471c7f.js"
+  }
+]);
